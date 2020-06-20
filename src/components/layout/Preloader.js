22b5dd8379eb10b1e44d68f7de71ea21110d6526
pstrassmann <st32490@gmail.com>
2020-06-19 17:00:57 -0700

    cleanup

--- src/components/layout/Preloader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Preloader = () => (
-  <div className="progress blue lighten-4">
-    <div className="indeterminate blue" />
-  </div>
-);
-
-export default Preloader;
