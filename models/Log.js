1b5cbb02a4330c6a277839d5ee47fa061627d789
pstrassmann <st32490@gmail.com>
2020-06-23 12:47:04 -0700

    Backend api server and routes created and working in postman

---
+++ models/Log.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const LogSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  attention: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('log', LogSchema);
