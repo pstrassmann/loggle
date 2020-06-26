c60367886cd19c6532ca3a7174282ae9f8ed37e5
pstrassmann <st32490@gmail.com>
2020-06-25 17:31:08 -0700

    Tech list modal no longer pings server for techs on component mount

--- client/src/components/techs/TechListModal.js
+++ client/src/components/techs/TechListModal.js
@@ -1,20 +1,15 @@
-import React, {useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { getTechs } from '../../actions/techActions';
 import TechItem from './TechItem';
 
 const TechListModal = ({ techs, getTechs }) => {
-  useEffect(() => {
-    getTechs();
-    // eslint-disable-next-line
-  }, []);
-
   return (
     <div id="tech-list-modal" className="modal">
       <div className="modal-content">
         <h4>Authorized Technicians</h4>
         <ul className="collection">
-          {techs && techs.map((tech) => <TechItem key={tech.id} tech={tech} />)}
+          {techs && techs.map((tech) => <TechItem key={tech._id} tech={tech} />)}
         </ul>
       </div>
     </div>
