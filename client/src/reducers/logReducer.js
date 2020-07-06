5ea3cef5f5fd5c254e493d28392c242a717f5db1
pstrassmann <st32490@gmail.com>
2020-07-06 14:39:19 -0700

    New added logs now show first instead of last

--- client/src/reducers/logReducer.js
+++ client/src/reducers/logReducer.js
@@ -40,7 +40,7 @@ export default (state = initialState, action) => {
     case ADD_LOG:
       return {
         ...state,
-        logs: [...state.logs, action.payload],
+        logs: [action.payload, ...state.logs],
         loading: false,
       };
     case DELETE_LOG:
